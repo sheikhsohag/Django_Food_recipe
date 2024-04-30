9e703b1be19d6e652e65336f133a3c83346e1b4c
sheikhsohag <sheikhsohag000@gmail.com>
2024-04-30 19:27:21 +0600

    home.html problem solved1

--- store/views.py
+++ store/views.py
@@ -11,11 +11,11 @@ from store.models import Product
 from category.models import Category
 
 
-def home(request):
-    product = Product.objects.all()
-    cat = Category.objects.all()
-    context =  {'product' : product, 'category':cat}
-    return render(request,'index.html', context)
+# def home(request):
+#     product = Product.objects.all()
+#     cat = Category.objects.all()
+#     context =  {'product' : product, 'category':cat}
+#     return render(request,'index.html', context)
 
 # ==============================================================
 
